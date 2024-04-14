fa598b6427ed26a4bb008936e9c7fb8218e762bd
brunomcr <bruno.mcreal@gmailcom>
2024-04-14 12:18:21 -0300

    Loading CSV files to MongoDB

---
+++ load_to_db.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from db_connection import connect_to_db
+import os
+
+# Connect to MongoDB / Conexão ao MongoDB
+db = connect_to_db('1mlet_embrapa')
+
+# Directory that contains the CSV files / Diretório que contém os arquivos CSV
+csv_directory = 'csv_files'
+
+# List of CSV files / Lista de arquivos CSV
+csv_files = [f for f in os.listdir(csv_directory) if f.endswith('.csv')]
+
+# Load each CSV file into a MongoDB collection
+# Carregar cada arquivo CSV para uma coleção no MongoDB
+for file in csv_files:
+    collection_name = file.split('.')[0]  # Collection name based on the file name
+    df = pd.read_csv(os.path.join(csv_directory, file), sep=';')
+    db[collection_name].insert_many(df.to_dict('records'))
+
+print("Data loaded successfully!")
