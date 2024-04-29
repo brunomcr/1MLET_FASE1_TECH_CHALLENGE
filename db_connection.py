9ae8e77b6c71b3ff29c842ef8a58e12e20769ce7
brunomcr <bruno.mcreal@gmailcom>
2024-04-29 13:18:52 -0300

    Conexacao ao mongo/docker e mongo/local

--- db_connection.py
+++ db_connection.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+from pymongo.errors import ServerSelectionTimeoutError
 
 
-def connect_to_db(db_name):
+def connect_to_local_db(db_name):
     """
     Connects to DB and return a database object
     :param db_name: str, name of the db
@@ -10,3 +13,36 @@ def connect_to_db(db_name):
     client = MongoClient('mongodb://localhost:27017/')  # ⚠️ Need to change "localhost" to service name in Docker
     db = client[db_name]
     return db
+
+
+def connect_to_docker_db(db_name):
+
+    # Carrega .env
+    load_dotenv()
+
+    # Pega variaveis de ambient (.env)
+    mongo_user = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+    mongo_pass = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+    print(f"Mongo User: {mongo_user}")
+    print(f"Mongo Pass: {mongo_pass}")
+
+    # Gera uri para conexao
+    uri = f"mongodb://{mongo_user}:{mongo_pass}@mongo-db:27017/"
+    print(f"uri: {uri}")
+
+    # tenta conecatar no banco
+    try:
+        client = MongoClient(uri, serverSelectionTimeoutMS=10000)
+        # Verifica informacoes da conexao
+        client.server_info()
+    except ServerSelectionTimeoutError as e:
+        print(f"Erro ao conectar ao MongoDB: {e}")
+        return None
+
+    # Seleciona o banco de dados.
+    db = client[db_name]
+    print(f"db: {db}")
+
+    return db
+
+
