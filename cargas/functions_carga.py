19c80a61dc2da87c38bb324fec1b57e3405d7ec5
rodrigo.santili <rodrigo.santili@v8.tech>
2024-05-08 15:05:26 -0300

    Parallelizing with "ThreadPoolExecutor" and added timeout when fetching data

--- cargas/functions_carga.py
+++ cargas/functions_carga.py
@@ -3,212 +3,184 @@ from bs4 import BeautifulSoup
 import json
 import os
 
-# A base da URL
+# URL base
 base_url = 'http://vitibrasil.cnpuv.embrapa.br/index.php'
 
-# Caminho para a pasta onde os arquivos JSON serão salvos
-json_files_dir = 'json_files/'
+# Json files directory
+json_files_dir = 'cargas/json_files/'
 if not os.path.exists(json_files_dir):
     os.mkdir(json_files_dir)
 
 
-def get_data(opt, subopt, file_name, data_inicio, data_fim):
+def get_data(opt, subopt, file_name, start_year, end_year):
     if opt == 'opt_05' and subopt in ('subopt_01', 'subopt_02', 'subopt_03', 'subopt_04', 'subopt_05') \
     or opt == 'opt_06' and subopt in ('subopt_01', 'subopt_02', 'subopt_03', 'subopt_04'): 
-        # Dicionário de opções e subopções com os nomes dos arquivos correspondentes
-        opcoes_subopcoes_arquivos = {
+        opt_subopt_files = {
         (opt, subopt): file_name,
         }
 
-        # Itera pelas combinações de opções e subopções
-        for (opcao, subopcao), arquivo in opcoes_subopcoes_arquivos.items():
-            todos_dados = []  # Inicializa a lista de dados para cada combinação
+        for (opt, subopt), files in opt_subopt_files.items():
+            all_data = []  
 
-            for ano in range(data_inicio, data_fim):
-                # Os parâmetros da URL.
+            for year in range(start_year, end_year):
                 params = {
-                    'ano': ano,
-                    'opcao': opcao,
-                    'subopcao': subopcao
+                    'ano': year,
+                    'opcao': opt,
+                    'subopcao': subopt
                 }
                 
-                # Faz a requisição para o site com os parâmetros do ano específico.
-                response = requests.get(base_url, params=params)
+                response = requests.get(base_url, params=params, timeout=10000)
                 soup = BeautifulSoup(response.text, 'html.parser')
 
-                # Procura pela tabela com a classe específica.
-                tabela = soup.find('table', class_='tb_base tb_dados')
+                table = soup.find('table', class_='tb_base tb_dados')
 
-                # Verifica se a tabela foi encontrada antes de continuar.
-                if tabela:
-                    # A lista de linhas da tabela
-                    linhas = tabela.find_all('tr')[1:]  # Pula o cabeçalho.
+                if table:
+                    lines = table.find_all('tr')[1:] 
                     
-                    # Itera pelas linhas da tabela, excluindo a última linha que contém o total.
-                    for linha in linhas[:-1]:
-                        colunas = linha.find_all('td')
-                        linha_dados = {
-                            "Ano": str(ano),
-                            "Pais": colunas[0].text.strip(),
-                            "Quantidade": colunas[1].text.strip().replace("-", "0"),
-                            "Valor": colunas[2].text.strip().replace("-", "0")
+                    for lines in lines[:-1]:
+                        coluns = lines.find_all('td')
+                        data_lines = {
+                            "Ano": str(year),
+                            "Pais": coluns[0].text.strip(),
+                            "Quantidade": coluns[1].text.strip().replace("-", "0"),
+                            "Valor": coluns[2].text.strip().replace("-", "0")
                         }
-                        if linha_dados["Pais"]:  # Certifica-se de que a linha contém o nome do país.
-                            todos_dados.append(linha_dados)
+                        if data_lines["Pais"]:
+                            all_data.append(data_lines)
             
-            # Converter a lista de dicionários em JSON e escrever no arquivo específico
-            with open(json_files_dir + arquivo, 'w', encoding='utf-8') as json_file:
-                json.dump(todos_dados, json_file, ensure_ascii=False, indent=4)
+            with open(json_files_dir + files, 'w', encoding='utf-8') as json_file:
+                json.dump(all_data, json_file, ensure_ascii=False, indent=4)
 
-            print(f"Arquivo {arquivo} criado com sucesso.")
+            print(f"{files} file was created successfully!")
     
     elif opt == 'opt_02' and subopt == None \
     or opt == 'opt_04' and subopt == None:
-        # Inicializa a lista de dados
-        todos_dados = []  
+        all_data = []  
 
-        for ano in range(data_inicio, data_fim):
-            # Os parâmetros da URL
+        for year in range(start_year, end_year):
             params = {
-                'ano': ano,
+                'ano': year,
                 'opcao': opt,
                 'subopcao': subopt
             }
             
-            # Faz a requisição para o site com os parâmetros do ano específico
-            response = requests.get(base_url, params=params)
+            response = requests.get(base_url, params=params, timeout=10000)
             soup = BeautifulSoup(response.text, 'html.parser')
             
-            # Procura pela tabela com a classe específica
-            tabela = soup.find('table', class_='tb_base tb_dados')
+            table = soup.find('table', class_='tb_base tb_dados')
 
-            # Verifica se a tabela foi encontrada antes de continuar
-            if tabela:
-                grupo_atual = ""  # Inicializa o grupo atual
+            if table:
+                current_group = ""  
 
-                # Itera pelas linhas da tabela
-                for linha in tabela.find_all('tr'):
-                    item = linha.find('td', class_='tb_item')
-                    subitem = linha.find('td', class_='tb_subitem')
+                for lines in table.find_all('tr'):
+                    item = lines.find('td', class_='tb_item')
+                    subitem = lines.find('td', class_='tb_subitem')
 
                     if item:
-                        grupo_atual = item.text.strip()  # Atualiza o grupo atual quando um novo tb_item é encontrado
+                        current_group = item.text.strip()
                     
-                    if subitem and grupo_atual:  # Processa subitens se um grupo está definido
-                        produto = subitem.text.strip()
-                        quantidade_l = subitem.find_next_sibling('td').text.strip().replace("-", "0")
+                    if subitem and current_group: 
+                        product = subitem.text.strip()
+                        volume = subitem.find_next_sibling('td').text.strip().replace("-", "0")
                         dados = {
-                            "Ano": str(ano),
-                            "Grupo": grupo_atual,
-                            "Produto": produto,
-                            "QuantidadeL": quantidade_l
+                            "Ano": str(year),
+                            "Grupo": current_group,
+                            "Produto": product,
+                            "QuantidadeL": volume
                         }
-                        todos_dados.append(dados)
+                        all_data.append(dados)
 
         # Converter a lista de dicionários em JSON e escrever no arquivo específico
-        arquivo_caminho = json_files_dir + file_name
-        with open(arquivo_caminho, 'w', encoding='utf-8') as json_file:
-            json.dump(todos_dados, json_file, ensure_ascii=False, indent=4)
+        file_path = json_files_dir + file_name
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(all_data, json_file, ensure_ascii=False, indent=4)
 
-        print(f"Arquivo {file_name} criado com sucesso.")
+        print(f"{file_name} file was created successfully!")
     
     elif opt == 'opt_03' and subopt in ('subopt_01', 'subopt_02', 'subopt_03'):
-        # Dicionário de opções e subopções com os nomes dos arquivos correspondentes
-        opcoes_subopcoes_arquivos = {
+
+        opt_subopt_files = {
             (opt, subopt): file_name,
         }
-        # Itera pelas combinações de opções e subopções
-        for (opcao, subopcao), arquivo in opcoes_subopcoes_arquivos.items():
-            todos_dados = []  # Inicializa a lista de dados para cada combinação
+        
+        for (opt, subopt), files in opt_subopt_files.items():
+            all_data = [] 
 
-            for ano in range(data_inicio, data_fim):
-                # Os parâmetros da URL.
+            for year in range(start_year, end_year):
                 params = {
-                    'ano': ano,
-                    'opcao': opcao,
-                    'subopcao': subopcao
+                    'ano': year,
+                    'opcao': opt,
+                    'subopcao': subopt
                 }
                 
-                # Faz a requisição para o site com os parâmetros do ano específico.
-                response = requests.get(base_url, params=params)
+                response = requests.get(base_url, params=params, timeout=10000)
                 soup = BeautifulSoup(response.text, 'html.parser')
 
-                # Procura pela tabela com a classe específica.
-                tabela = soup.find('table', class_='tb_base tb_dados')
+                table = soup.find('table', class_='tb_base tb_dados')
 
-                # Verifica se a tabela foi encontrada antes de continuar.
-                if tabela:
-                    grupo_atual = ""
-                    # Itera pelas linhas da tabela.
-                    for linha in tabela.find_all('tr'):
-                        item = linha.find('td', class_='tb_item')
-                        subitem = linha.find('td', class_='tb_subitem')
+                if table:
+                    current_group = ""
+                    for lines in table.find_all('tr'):
+                        item = lines.find('td', class_='tb_item')
+                        subitem = lines.find('td', class_='tb_subitem')
                         
                         if item:
-                            grupo_atual = item.text.strip()  # Define o grupo atual
+                            current_group = item.text.strip() 
                             
-                        if subitem and grupo_atual:  # Se 'tb_subitem' e temos um grupo atual definido
-                            cultivar = subitem.text.strip()
-                            # A próxima 'td' deve conter a quantidade
-                            quantidade_kg = subitem.find_next_sibling('td').text.strip().replace("-", "0")
+                        if subitem and current_group:
+                            cultivate = subitem.text.strip()
+                            qty_kg = subitem.find_next_sibling('td').text.strip().replace("-", "0")
                             dados = {
-                                "Ano": str(ano),
-                                "Grupo": grupo_atual,
-                                "Cultivar": cultivar,
-                                "QuantidadeKg": quantidade_kg
+                                "Ano": str(year),
+                                "Grupo": current_group,
+                                "Cultivar": cultivate,
+                                "QuantidadeKg": qty_kg
                             }
-                            todos_dados.append(dados)
+                            all_data.append(dados)
 
-            # Converter a lista de dicionários em JSON e escrever no arquivo específico
-            arquivo_caminho = json_files_dir + arquivo
-            with open(arquivo_caminho, 'w', encoding='utf-8') as json_file:
-                json.dump(todos_dados, json_file, ensure_ascii=False, indent=4)
+            file_path = json_files_dir + files
+            with open(file_path, 'w', encoding='utf-8') as json_file:
+                json.dump(all_data, json_file, ensure_ascii=False, indent=4)
 
-            print(f"Arquivo {arquivo} criado com sucesso.")
+            print(f"{files} file was created successfully!")
 
     elif opt == 'opt_03' and subopt == 'subopt_04':
-        # Define o arquivo para os dados de uvas sem classificação
-        todos_dados = []  # Inicializa a lista de dados
 
-        for ano in range(data_inicio, data_fim):  # Ajuste a faixa de anos conforme necessário
-            # Os parâmetros da URL
+        all_data = []  
+
+        for year in range(start_year, end_year):  
             params = {
-                'ano': ano,
+                'ano': year,
                 'opcao': opt,
                 'subopcao': subopt
             }
             
-            # Faz a requisição para o site com os parâmetros do ano específico
-            response = requests.get(base_url, params=params)
+            response = requests.get(base_url, params=params, timeout=10000)
             soup = BeautifulSoup(response.text, 'html.parser')
             
-            # Procura pela tabela com a classe específica
-            tabela = soup.find('table', class_='tb_base tb_dados')
+            table = soup.find('table', class_='tb_base tb_dados')
 
-            # Verifica se a tabela foi encontrada antes de continuar
-            if tabela:
-                linhas = tabela.find_all('tr')
-                # Itera pelas linhas da tabela, excluindo o cabeçalho e o rodapé (total)
-                for linha in linhas[1:-1]:  # Pula a primeira linha (cabeçalho) e ignora a última linha (total)
-                    colunas = linha.find_all('td')
-                    if colunas and len(colunas) >= 2:
-                        sem_definicao = colunas[0].text.strip()
-                        quantidade_kg = colunas[1].text.strip().replace("-", "0")
+            if table:
+                lines = table.find_all('tr')
+
+                for lines in lines[1:-1]:  
+                    coluns = lines.find_all('td')
+                    if coluns and len(coluns) >= 2:
+                        sem_definicao = coluns[0].text.strip()
+                        qty_kg = coluns[1].text.strip().replace("-", "0")
                         dados = {
-                            "Ano": str(ano),
+                            "Ano": str(year),
                             "SemDefinicao": sem_definicao,
-                            "QuantidadeKg": quantidade_kg
+                            "QuantidadeKg": qty_kg
                         }
-                        todos_dados.append(dados)
+                        all_data.append(dados)
 
-        # Converter a lista de dicionários em JSON e escrever no arquivo específico
-        arquivo_caminho = json_files_dir + file_name
-        with open(arquivo_caminho, 'w', encoding='utf-8') as json_file:
-            json.dump(todos_dados, json_file, ensure_ascii=False, indent=4)
 
-        print(f"Arquivo {file_name} criado com sucesso.")
-   
+        file_path = json_files_dir + file_name
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(all_data, json_file, ensure_ascii=False, indent=4)
+
+        print(f"{file_name} file was created successfully!")
+
     else:
-        print(f'A combinação entre a opção "{opt}" e subopcao "{subopt}" não existe')
-
-
+        print(f'The combination of parameters "{opt}" and "{subopt}" does not exist')
--- cargas/parallel_web_scraper.py
+++ cargas/parallel_web_scraper.py
@@ -4,34 +4,29 @@ from functions_carga import get_data
 _START_DATE = 1970
 _LAST_DATE = 2024
 
-# Opt e Subopt de cada pagina e o nome do arquivo.
 tasks = [
+    ('opt_02', None, 'Producao.json'),
+    ('opt_03', 'subopt_01', 'ProcessaViniferas.json'),
+    ('opt_03', 'subopt_02', 'ProcessaAmericanas.json'),
+    ('opt_03', 'subopt_03', 'ProcessaMesa.json'),
+    ('opt_03', 'subopt_04', 'ProcessaSemclass.json'),
     ('opt_04', None, 'Comercio.json'),
-    ('opt_06', 'subopt_02', 'ExpEspumantes.json'),
-    ('opt_06', 'subopt_04', 'ExpSuco.json'),
-    ('opt_06', 'subopt_03', 'ExpUva.json'),
-    ('opt_06', 'subopt_01', 'ExpVinho.json'),
+    ('opt_05', 'subopt_01', 'ImpVinhos.json'),
     ('opt_05', 'subopt_02', 'ImpEspumantes.json'),
     ('opt_05', 'subopt_03', 'ImpFrescas.json'),
     ('opt_05', 'subopt_04', 'ImpPassas.json'),
     ('opt_05', 'subopt_05', 'ImpSuco.json'),
-    ('opt_05', 'subopt_01', 'ImpVinhos.json'),
-    ('opt_03', 'subopt_02', 'ProcessaAmericanas.json'),
-    ('opt_03', 'subopt_03', 'ProcessaMesa.json'),
-    ('opt_03', 'subopt_04', 'ProcessaSemclass.json'),
-    ('opt_03', 'subopt_01', 'ProcessaViniferas.json'),
-    ('opt_02', None, 'Producao.json')
+    ('opt_06', 'subopt_01', 'ExpVinho.json'),
+    ('opt_06', 'subopt_02', 'ExpEspumantes.json'),
+    ('opt_06', 'subopt_04', 'ExpSuco.json'),
+    ('opt_06', 'subopt_03', 'ExpUva.json')    
 ]
 
-
-# Função para executar get_data
 def execute_get_data(option, suboption, filename):
     return get_data(option, suboption, filename, _START_DATE, _LAST_DATE)
 
 
-# ThreadPoolExecutor para executar as tarefas em paralelo
 with ThreadPoolExecutor() as executor:
-    # Dicionário para armazenar futures
     future_to_task = {executor.submit(execute_get_data, *task): task for task in tasks}
 
     for future in as_completed(future_to_task):
@@ -40,3 +35,45 @@ with ThreadPoolExecutor() as executor:
             data = future.result()
         except Exception as exc:
             print(f"Task {task} generated an exception: {exc}")
+
+
+
+# import argparse
+# from datetime import datetime
+
+# def main():
+#     parser = argparse.ArgumentParser(description='Execute get_data function with given parameters.')
+#     parser.add_argument('--option', type=str, help='Option parameter for get_data')
+#     parser.add_argument('--suboption', type=str, default=None, help='Suboption parameter for get_data (optional)')
+#     parser.add_argument('--filename', type=str, help='Filename parameter for get_data')
+#     parser.add_argument('--start_date', type=int, default=1970, help='Start date for data fetching')
+#     parser.add_argument('--end_date', type=int, default=datetime.now().year, help='End date for data fetching')
+
+
+#     args = parser.parse_args()
+
+#     result = get_data(args.option, args.suboption, args.filename, args.start_date, args.end_date)
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+# '''
+# python cargas/parallel_web_scraper.py --option=opt_02 --filename=Producao.json &
+# python cargas/parallel_web_scraper.py --option=opt_04 --filename=Comercio.json &
+# python cargas/parallel_web_scraper.py --option=opt_06 --suboption=subopt_02 --filename=ExpEspumantes.json &
+# python cargas/parallel_web_scraper.py --option=opt_06 --suboption=subopt_04 --filename=ExpSuco.json &
+# python cargas/parallel_web_scraper.py --option=opt_06 --suboption=subopt_03 --filename=ExpUva.json &
+# python cargas/parallel_web_scraper.py --option=opt_06 --suboption=subopt_01 --filename=ExpVinho.json &
+# python cargas/parallel_web_scraper.py --option=opt_05 --suboption=subopt_02 --filename=ImpEspumantes.json &
+# python cargas/parallel_web_scraper.py --option=opt_05 --suboption=subopt_03 --filename=ImpFrescas.json &
+# python cargas/parallel_web_scraper.py --option=opt_05 --suboption=subopt_04 --filename=ImpPassas.json &
+# python cargas/parallel_web_scraper.py --option=opt_05 --suboption=subopt_05 --filename=ImpSuco.json &
+# python cargas/parallel_web_scraper.py --option=opt_05 --suboption=subopt_01 --filename=ImpVinhos.json &
+# python cargas/parallel_web_scraper.py --option=opt_03 --suboption=subopt_02 --filename=ProcessaAmericanas.json &
+# python cargas/parallel_web_scraper.py --option=opt_03 --suboption=subopt_03 --filename=ProcessaMesa.json &
+# python cargas/parallel_web_scraper.py --option=opt_03 --suboption=subopt_04 --filename=ProcessaSemclass.json &
+# python cargas/parallel_web_scraper.py --option=opt_03 --suboption=subopt_01 --filename=ProcessaViniferas.json &
+# '''
